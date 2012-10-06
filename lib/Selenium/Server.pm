722552a6c5fb3ac2b0318eed77d1b53b6859d4fe
ikasam_a <masaki.nakagawa@gmail.com>
2012-10-06 21:18:31 +0900

    initial commit

---
+++ lib/Selenium/Server.pm
@@ -0,0 +1,37 @@
+package Selenium::Server;
+
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+$VERSION = eval $VERSION;
+
+1;
+
+=head1 NAME
+
+Selenium::Server - A module that ...
+
+=head1 SYNOPSIS
+
+    use Selenium::Server;
+
+=head1 DESCRIPTION
+
+Selenium::Server is
+
+=head1 METHODS
+
+=head1 AUTHOR
+
+NAKAGAWA Masaki E<lt>masaki@cpan.orgE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
